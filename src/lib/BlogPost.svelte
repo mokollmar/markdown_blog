6db20f5e5f1e6b96307833f26063f3b92a087953
@mokollmar <hi@mokollmar.com>
2024-11-23 16:47:29 +0100

    added article style

--- src/lib/BlogPost.svelte
+++ src/lib/BlogPost.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+    import "./post.css"
 </script>
 
 <div
-    class="text-overred-variant-blue flex w-full flex-col space-y-5 text-center"
+    class="text-overred-variant-blue flex w-full flex-col text-center"
 >
     <slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,9 @@
 	});
 </script>
 
-<div class="flex flex-col p-12">
+<div class="flex flex-col justify-center items-center space-y-12">
 	<h1 class="text-center font-bold text-5xl">{data.props?.meta.title}</h1>
-	<article
-		id="blog-article"
-		style="scroll-behavior: smooth; scroll-padding: 50px;"
-		class="prose mx-8 mb-3 flex w-full max-w-none grow justify-start rounded-xl bg-white pt-0 sm:mx-auto sm:border-4 sm:p-20 m-12"
-	>
+	<article class="prose w-full">
 		<svelte:component this={data?.props?.content} />
 	</article>
 </div>
